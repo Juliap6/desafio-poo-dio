007796987ac6e0b88d0fa2e58c875707eafdc0e6
Prates <jdspbr@hotmail.com>
2022-08-08 23:19:49 -0300

    commit2

--- src/main/java/br/com/dio/desafio/dominio/Main.java
+++ src/main/java/br/com/dio/desafio/dominio/Main.java
@@ -14,6 +14,8 @@ public class Main {
         curso2.setDescricao("descricao curso js");
         curso2.setCargaHoraria(4);
 
+
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descricao mentoria java");
